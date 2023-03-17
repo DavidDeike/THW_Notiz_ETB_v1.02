b638b6b9817765d51c71103d7ff0dc7bff138562
David Deike <117454601+DavidDeike@users.noreply.github.com>
2023-03-17 17:21:22 +0100

    Feierabend

--- app/src/main/java/com/example/thwnotizetb/CalenderFragment.kt
+++ app/src/main/java/com/example/thwnotizetb/CalenderFragment.kt
@@ -1,11 +1,15 @@
 package com.example.thwnotizetb
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.CalendarContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
+import androidx.fragment.app.Fragment
 import com.example.thwnotizetb.databinding.FragmentCalenderBinding
+import java.util.*
 
 
 class CalenderFragment : Fragment() {
@@ -24,7 +28,38 @@ class CalenderFragment : Fragment() {
     }
 
 
-//komentar
+    val cal = Calendar.getInstance()
+
+    val title = binding.etTermin
+    val beschreibung = binding.etBeschreibung
+    val ort = binding.etOrt
+
+    val hinzufuegen = binding.addBt
+
+    .setOnClickListener {
+        addEvent(title.text.toString(), ort.text.toString(), beschreibung.text.toString(), cal.timeInMillis, cal.timeInMillis + 1800000);
+    }
+
+}
+
+fun addEvent(title: String, location: String, description: String, begin: Long, end: Long) {
+
+    val intent = Intent(Intent.ACTION_INSERT)
+
+
+        .putExtra(CalendarContract.Events.TITLE, title)
+        .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+        .putExtra(CalendarContract.Events.DESCRIPTION, description)
+        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin)
+        .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end)
+        .setDataAndType(CalendarContract.Events.CONTENT_URI,"vnd.android.cursor.dir/event" )
+
+
+
+    startActivity(intent)
+}
+
+
 
 
 
